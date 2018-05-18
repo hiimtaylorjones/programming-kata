c8147dbd8b9ab5795d918a64a444a01e1f92c046
hiimtaylorjones <taylorjonessoftware@gmail.com>
2018-05-18 10:12:03 -0400

    solution with passing tests

--- karate/main.rb
+++ karate/main.rb
@@ -1,13 +1,21 @@
 require "test/unit/assertions"
 include Test::Unit::Assertions
 
-def chop(int, array_of_int)
+def chop(int, array)
+  array.each_index do |index|
+    if array[index] == int
+      return index
+    end
+  end
+  return -1
 end
 
 def test_chop
+  puts "STARTING TESTS"
   assert_equal(-1, chop(3, []))
   assert_equal(-1, chop(3, [1]))
   assert_equal(0,  chop(1, [1]))
+  puts "Part 1 Passed"
   #
   assert_equal(0,  chop(1, [1, 3, 5]))
   assert_equal(1,  chop(3, [1, 3, 5]))
@@ -16,6 +24,7 @@ def test_chop
   assert_equal(-1, chop(2, [1, 3, 5]))
   assert_equal(-1, chop(4, [1, 3, 5]))
   assert_equal(-1, chop(6, [1, 3, 5]))
+  puts "Part 2 Passed"
   #
   assert_equal(0,  chop(1, [1, 3, 5, 7]))
   assert_equal(1,  chop(3, [1, 3, 5, 7]))
@@ -26,6 +35,7 @@ def test_chop
   assert_equal(-1, chop(4, [1, 3, 5, 7]))
   assert_equal(-1, chop(6, [1, 3, 5, 7]))
   assert_equal(-1, chop(8, [1, 3, 5, 7]))
+  puts "ALL TESTS PASSED"
 end
 
 test_chop
