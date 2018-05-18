f4d588fade867686dbf08bf79bf14bfe58fd08b5
hiimtaylorjones <taylorjonessoftware@gmail.com>
2018-05-18 15:18:50 -0400

    green tests

--- karate/rust/src/lib.rs
+++ karate/rust/src/lib.rs
@@ -24,22 +24,22 @@ mod tests {
         assert_eq!(-1, chop(3, &mut [1]));
         assert_eq!(0,  chop(1, &mut [1]));
         //
-        // assert_eq!(0,  chop(1, [1, 3, 5]));
-        // assert_eq!(1,  chop(3, [1, 3, 5]));
-        // assert_eq!(2,  chop(5, [1, 3, 5]));
-        // assert_eq!(-1, chop(0, [1, 3, 5]));
-        // assert_eq!(-1, chop(2, [1, 3, 5]));
-        // assert_eq!(-1, chop(4, [1, 3, 5]));
-        // assert_eq!(-1, chop(6, [1, 3, 5]));
+        assert_eq!(0,  chop(1, &mut [1, 3, 5]));
+        assert_eq!(1,  chop(3, &mut [1, 3, 5]));
+        assert_eq!(2,  chop(5, &mut [1, 3, 5]));
+        assert_eq!(-1, chop(0, &mut [1, 3, 5]));
+        assert_eq!(-1, chop(2, &mut [1, 3, 5]));
+        assert_eq!(-1, chop(4, &mut [1, 3, 5]));
+        assert_eq!(-1, chop(6, &mut [1, 3, 5]));
         // //
-        // assert_eq!(0,  chop(1, [1, 3, 5, 7]));
-        // assert_eq!(1,  chop(3, [1, 3, 5, 7]));
-        // assert_eq!(2,  chop(5, [1, 3, 5, 7]));
-        // assert_eq!(3,  chop(7, [1, 3, 5, 7]));
-        // assert_eq!(-1, chop(0, [1, 3, 5, 7]));
-        // assert_eq!(-1, chop(2, [1, 3, 5, 7]));
-        // assert_eq!(-1, chop(4, [1, 3, 5, 7]));
-        // assert_eq!(-1, chop(6, [1, 3, 5, 7]));
-        // assert_eq!(-1, chop(8, [1, 3, 5, 7]));
+        assert_eq!(0,  &mut chop(1, [1, 3, 5, 7]));
+        assert_eq!(1,  &mut chop(3, [1, 3, 5, 7]));
+        assert_eq!(2,  &mut chop(5, [1, 3, 5, 7]));
+        assert_eq!(3,  &mut chop(7, [1, 3, 5, 7]));
+        assert_eq!(-1, &mut chop(0, [1, 3, 5, 7]));
+        assert_eq!(-1, &mut chop(2, [1, 3, 5, 7]));
+        assert_eq!(-1, &mut chop(4, [1, 3, 5, 7]));
+        assert_eq!(-1, &mut chop(6, [1, 3, 5, 7]));
+        assert_eq!(-1, &mut chop(8, [1, 3, 5, 7]));
     }
 }
