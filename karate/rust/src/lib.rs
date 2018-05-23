c99b30e4b1bea9dd16ad17e585125c7fc59ceba9
hiimtaylorjones <taylorjonessoftware@gmail.com>
2018-05-22 22:41:22 -0400

    getting there....

--- karate/rust/src/lib.rs
+++ karate/rust/src/lib.rs
@@ -12,29 +12,17 @@ pub fn chop(int: i32, array: &mut [i32]) -> i32{
 }
 
 pub fn binary_chop(int: i32, array: &mut [i32]) -> i32 {
-    let half = (array.len() / 2) as i32;
-    let full = array.len() as i32;
-
-    println!("{:?}", half);
-    println!("{:?}", full);
-    thread::spawn(|| {
-        let mut count = 0;
-        for x in 0..half {
-            let index = x as usize;
-            if array[index] == int {
-                return index;
-            }
+    let half = array.len() / 2;
+    for x in 0..half {
+        if array[x] == int {
+            return x as i32;
         }
-    });
-    thread::spawn(|| {
-        let mut count = 0;
-        for x in half..full {
-            let index = x as usize;
-            if array[index] == int {
-                return index;
-            }
+    }
+    for x in 0..array.len() {
+        if array[x] == int {
+            return x as i32;
         }
-    });
+    }
     return -1;
 }
 
@@ -73,26 +61,20 @@ mod tests {
 
     #[test]
     fn test_binary_chop() {
-        assert_eq!(-1, binary_chop(3, &mut []));
-        assert_eq!(-1, binary_chop(3, &mut [1]));
-        assert_eq!(0,  binary_chop(1, &mut [1]));
-        //
-        assert_eq!(0,  chop(1, &mut [1, 3, 5]));
-        assert_eq!(1,  chop(3, &mut [1, 3, 5]));
-        assert_eq!(2,  chop(5, &mut [1, 3, 5]));
-        assert_eq!(-1, chop(0, &mut [1, 3, 5]));
-        assert_eq!(-1, chop(2, &mut [1, 3, 5]));
-        assert_eq!(-1, chop(4, &mut [1, 3, 5]));
-        assert_eq!(-1, chop(6, &mut [1, 3, 5]));
-        // // //
-        assert_eq!(0,  chop(1, &mut [1, 3, 5, 7]));
-        assert_eq!(1,  chop(3, &mut [1, 3, 5, 7]));
-        assert_eq!(2,  chop(5, &mut [1, 3, 5, 7]));
-        assert_eq!(3,  chop(7, &mut [1, 3, 5, 7]));
-        assert_eq!(-1, chop(0, &mut [1, 3, 5, 7]));
-        assert_eq!(-1, chop(2, &mut [1, 3, 5, 7]));
-        assert_eq!(-1, chop(4, &mut [1, 3, 5, 7]));
-        assert_eq!(-1, chop(6, &mut [1, 3, 5, 7]));
-        assert_eq!(-1, chop(8, &mut [1, 3, 5, 7]));
+        let mut empty: [i32; 0] = [];
+        let mut one: [i32; 1] = [1];
+        // let mut one_three_five: [i32: 3] = [1, 3, 5];
+
+        assert_eq!(-1, binary_chop(3, &mut empty));
+        assert_eq!(-1, binary_chop(3, &mut one));
+        assert_eq!(0,  binary_chop(1, &mut one));
+
+        // assert_eq!(0,  chop(1, &mut one_three_five));
+        // assert_eq!(1,  chop(3, &mut one_three_five));
+        // assert_eq!(2,  chop(5, &mut one_three_five));
+        // assert_eq!(-1, chop(0, &mut one_three_five));
+        // assert_eq!(-1, chop(2, &mut one_three_five));
+        // assert_eq!(-1, chop(4, &mut one_three_five));
+        // assert_eq!(-1, chop(6, &mut one_three_five));
     }
 }
